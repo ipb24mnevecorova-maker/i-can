9ff565879478ce2ee436cd65c5fcd1bd6e81cb4a
Marta <ipb24.m.nevecorova@vtdt.edu.lv>
2026-05-13 14:37:48 +0300

    static skelet

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,27 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/archives', function () {
+    return view('archives');
+});
+Route::get('/cheatsheet', function () {
+    return view('cheatsheet');
+});
+Route::get('/topics', function () {
+    return view('topics');
+});
+Route::get('/calculus', function () {
+    return view('calculus');
+});
+Route::get('/feedback', function () {
+    return view('feedback');
+});
+Route::get('/contact', function () {
+    return view('contact');
+});
+Route::get('/about', function () {
+    return view('about');
+});
+Route::get('/newsletter', function () {
+    return view('newsletter');
 });
